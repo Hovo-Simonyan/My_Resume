5a0fb2413cc389c15ff43e74227a1d87ad31ab51
Hovo-Simonyan <97516485+Hovo-Simonyan@users.noreply.github.com>
2022-02-28 21:24:50 +0400

    Update settings.py

--- resume/resume/settings.py
+++ resume/resume/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
-SECRET_KEY = 'django-insecure-tu1zaexlk!p#sv^a@c%e(h#q*kj(v86^crw&+22v!mt1#zepf='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
